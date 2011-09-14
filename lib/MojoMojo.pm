5a5cffb7a3b57e4a4d2505efced18b4923b14d5e
Marcus Ramberg <marcus@nordaaker.com>
2011-09-14 18:00:52 +0800

    Fix broken tests, add canonical

--- lib/MojoMojo.pm
+++ lib/MojoMojo.pm
@@ -30,7 +30,7 @@ use Module::Pluggable::Ordered
     except      => qr/^MojoMojo::Plugin::/,
     require     => 1;
 
-our $VERSION = '1.04';
+our $VERSION = '1.05';
 use 5.008004;
 
 MojoMojo->config->{authentication}{dbic} = {
